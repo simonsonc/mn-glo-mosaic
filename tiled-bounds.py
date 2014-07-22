adbd0101764ed39e22ff952a5e3dd2601d1f61e8
Carl Simonson <simonsonc@gmail.com>
2014-07-21 20:01:29 -0500

    Discover tiles based on the cutlines

--- tiled-bounds.py
+++ tiled-bounds.py
@@ -1,65 +1,63 @@
 #!/usr/bin/env python
 from osgeo import ogr
+from osgeo import osr
 from osgeo import gdal
+import os.path
 
-def GetExtent(gt,cols,rows):
-    ''' Return list of corner coordinates from a geotransform
+t_srs = osr.SpatialReference()
+t_srs.ImportFromEPSG(26915)
 
-        @type gt:   C{tuple/list}
-        @param gt: geotransform
-        @type cols:   C{int}
-        @param cols: number of columns in the dataset
-        @type rows:   C{int}
-        @param rows: number of rows in the dataset
-        @rtype:    C{[float,...,float]}
-        @return:   coordinates of each corner
-    '''
-    ext=[]
-    xarr=[0,cols]
-    yarr=[0,rows]
-
-    for px in xarr:
-        for py in yarr:
-            x=gt[0]+(px*gt[1])+(py*gt[2])
-            y=gt[3]+(px*gt[4])+(py*gt[5])
-            ext.append([x,y])
-        yarr.reverse()
-    return ext
+s_srs = osr.SpatialReference()
+s_srs.ImportFromEPSG(4326)
 
 def raster_extents(raster):
-    ds=gdal.Open(raster)
+    ds=ogr.Open(raster)
 
-    gt=ds.GetGeoTransform()
-    cols = ds.RasterXSize
-    rows = ds.RasterYSize
-    ext=GetExtent(gt,cols,rows)
-    return ext
+    layer=ds.GetLayerByIndex(0)
+    left, right, top, bottom = layer.GetExtent()
 
-def contained(ext, x1, y1):
-    mx1 = min(ext[0][0], ext[1][0], ext[2][0], ext[3][0])
-    mx2 = max(ext[0][0], ext[1][0], ext[2][0], ext[3][0])
-    my1 = min(ext[0][1], ext[1][1], ext[2][1], ext[3][1])
-    my2 = max(ext[0][1], ext[1][1], ext[2][1], ext[3][1])
+    feature = layer.GetNextFeature()
+    while feature:
+        geom = feature.GetGeometryRef().Clone()
+        geom.TransformTo(t_srs)
+        yield geom
 
-    x2, y2 = x1 + 10000, y1 + 10000
-    ret = not (x2 < mx1 or x1 > mx2 or y2 < my1 or y1 > my2)
-    return ret
+        feature = layer.GetNextFeature()
 
 extents = {}
 import glob
-for vrt in glob.glob('data/*.vrt'):
-    ext = raster_extents(vrt)
-    if ext:
+for outline in glob.glob('cutlines/*.json'):
+    for ext in raster_extents(outline):
+        fn, _ = os.path.splitext(os.path.basename(outline))
+        vrt = 'data/%s.vrt' % (fn,)
         extents[vrt] = ext
 
 for x1 in range(460000, 520000, 10000):
     for y1 in range(4920000, 4970000, 10000):
+        left = x1
+        top = y1
+        right = x1 + 10000
+        bottom = y1 + 10000
+
+        edge = ogr.Geometry(ogr.wkbLinearRing)
+        edge.AssignSpatialReference(t_srs)
+        edge.AddPoint(left, top)
+        edge.AddPoint(right, top)
+        edge.AddPoint(right, bottom)
+        edge.AddPoint(left, bottom)
+        edge.CloseRings()
+
+        cutline = ogr.Geometry(ogr.wkbPolygon)
+        cutline.AddGeometry(edge)
+
         entries = []
-        print("%s %s" % (x1, y1))
         for k, v in extents.iteritems():
-            if contained(v, x1, y1):
+            #print("Edge", str(cutline))
+            #print("V", str(v))
+            if not v.Disjoint(cutline):
                 entries.append(k)
 
+        print("%d %d (%d)" % (x1, y1, len(entries)))
         if len(entries):
             with open('tile-entries/%dx%d.txt' % (x1, y1), 'w') as out:
                 for i in entries:
