9bfba92196c3e94b8a58d9620f6e2385135bec68
Carl Simonson <simonsonc@gmail.com>
2014-07-26 08:22:06 -0500

    Record the command I tried to get all the files

--- get-county-data.sh
+++ get-county-data.sh
@@ -2,5 +2,6 @@
 while read line
 do
     echo $line
+    #curl -l "ftp://ftp.lmic.state.mn.us/pub/data/basemaps/glo/$line/Georeferenced/" | grep '.*i[0-9][0-9]\.zip$' > "counties/$line.txt"
     curl -l "ftp://ftp.lmic.state.mn.us/pub/data/basemaps/glo/$line/Georeferenced/" | grep ".*01.zip$" > "counties/$line.txt"
 done < counties.txt
