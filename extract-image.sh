44c86bd5ba3dcb1cdf704a64b8f7b714a413eaf1
Carl Simonson <simonsonc@gmail.com>
2014-08-02 16:36:13 -0500

    Make extract-image download the file

--- extract-image.sh
+++ extract-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bn=`basename $1`
-trsid=${bn%%.*}
-gdal_translate /vsizip/$1/${trsid}.jpg ${trsid}.tif
+trsid=$1
+rake data/${trsid}.zip
+gdal_translate /vsizip/data/${trsid}.zip/${trsid}.jpg ${trsid}.tif
 gdaladdo ${trsid}.tif 2 4 6 8 16
