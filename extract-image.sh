0e0b56af4beb2026000a0db14801f01cdd2929b6
Carl Simonson <simonsonc@gmail.com>
2014-08-02 20:22:07 -0500

    Reduce the size of the images to make things faster

--- extract-image.sh
+++ extract-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for trsid; do
     rake data/${trsid}.zip
-    gdal_translate /vsizip/data/${trsid}.zip/${trsid}.jpg ${trsid}.tif
+    gdal_translate -co TILED=YES -outsize 50% 50% /vsizip/data/${trsid}.zip/${trsid}.jpg ${trsid}.tif
     gdaladdo ${trsid}.tif 2 4 6 8 16
 done
