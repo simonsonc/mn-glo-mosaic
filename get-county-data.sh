261ac0e6ccf5a1729591cc62d285ab7e2b402dc4
Carl Simonson <simonsonc@gmail.com>
2014-08-19 20:23:09 -0500

    Remove old script for county data

--- get-county-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while read line
-do
-    echo $line
-    #curl -l "ftp://ftp.lmic.state.mn.us/pub/data/basemaps/glo/$line/Georeferenced/" | grep '.*i[0-9][0-9]\.zip$' > "counties/$line.txt"
-    curl -l "ftp://ftp.lmic.state.mn.us/pub/data/basemaps/glo/$line/Georeferenced/" | grep ".*01.zip$" > "counties/$line.txt"
-done < counties.txt
